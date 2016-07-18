7f47bf90ee5be5f719d50c173f3dcc4ea5185735
Stephen Lawrensen <slawrensen@gmail.com>
2016-07-18 17:53:20 -0500

    slim_jimmy7

--- botl/scripts/_references.js
+++ botl/scripts/_references.js
@@ -3,9 +3,9 @@
 /// <reference path="ai.0.22.9-build00167.js" />
 /// <reference path="angular.js" />
 /// <reference path="angular-mocks.js" />
+/// <reference path="angular-route.min.js" />
 /// <reference path="angular-ui/ui-bootstrap.js" />
 /// <reference path="angular-ui/ui-bootstrap-tpls.js" />
 /// <reference path="app.js" />
-/// <reference path="app2.js" />
 /// <reference path="bootstrap.js" />
 
---
+++ botl/scripts/app.js
@@ -0,0 +1,26 @@
+﻿// Define the `SharedHomeLab` module
+var botlApp = angular.module('breakOnTheLake', ['ui.bootstrap', 'ngRoute']);
+
+botlApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      // Home
+      .when("/", { templateUrl: "parts/home.html", controller: "PageCtrl" })
+      // Pages
+      .when("/about", { templateUrl: "parts/about.html", controller: "PageCtrl" })
+      .when("/gallery", { templateUrl: "parts/gallery.html", controller: "PageCtrl" })
+      .when("/rates", { templateUrl: "parts/rates.html", controller: "PageCtrl" })
+      .when("/amenities", { templateUrl: "parts/amenities.html", controller: "PageCtrl" })
+      .when("/contact", { templateUrl: "parts/contact.html", controller: "PageCtrl" })
+      // Blog
+      .when("/blog", { templateUrl: "parts/blog.html", controller: "BlogCtrl" })
+      .when("/blog/post", { templateUrl: "parts/blog_item.html", controller: "BlogCtrl" })
+      // else 404
+      .otherwise("/404", { templateUrl: "parts/404.html", controller: "PageCtrl" });
+}]);
+
+/**
+ * Controls all other Pages
+ */
+botlApp.controller('PageCtrl', function (/* $scope, $location, $http */) {
+    console.log("Page Controller reporting for duty.");
+});
